c908215111e2cba3e13bf447563890a0e4c9b27c
Brian Reisman <brianreisman@protonmail.com>
2020-12-01 16:28:31 -0500

    Added onClick attribute to JSX to return to home component

--- src/components/Header.js/Header.js
+++ src/components/Header.js/Header.js
@@ -1,8 +1,19 @@
 import HeaderStyles from './header-styled';
+import {useHistory} from 'react-router-dom';
 
 function Header(props) {
+  const {push} = useHistory();
+
+  function onBackClick(e){
+    console.log('back button clicked')
+    push('/');
+  }
+
+
+
   return (
     <HeaderStyles>
+      <div onClick={onBackClick}>back</div>
       <h1>{props.name}</h1>
     </HeaderStyles>
   );
--- src/components/Header.js/header-styled.js
+++ src/components/Header.js/header-styled.js
@@ -1,10 +1,34 @@
 import styled from 'styled-components';
 
 const HeaderStyles = styled.div`
+border-bottom: 2px solid #ccc;
+h1, div{
+border: 1px solid tomato;
+}
 display: flex;
-button{
-  padding: 0;
-  margin: 0;
+width: 90%;
+justify-content: space-between;
+align-items: center;
+div{
+  padding: 1%;
+  background-color: rose;
+  border-radius: 0 30px 0 30px;
+  transition-property: all 1s;
+  &::before{
+    transition: 1s;
+    content: '';
+    display: none;
+  }
+  &:hover{
+    background-color: tomato;
+    &::before{
+      content: '*';
+      color: white;
+      display: inline;
+      background-color: black;
+      border-radius: 50px;
+    }
+  }
 }
   h1{
     font-size: 3rem;
